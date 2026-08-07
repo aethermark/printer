d29c91a541ebc43eef8259cf87700dedbed2b7f1
Mukul Waval <mukul.waval@gmail.com>
2026-08-07 06:53:59 +0000

    fix: rename instances of glyph to printer

--- cmake/EnableCoverage.cmake
+++ cmake/EnableCoverage.cmake
@@ -1,5 +1,5 @@
 function(enable_coverage target)
-    if(GLYPH_ENABLE_COVERAGE)
+  if(PRINTER_ENABLE_COVERAGE)
         target_compile_options(${target} PRIVATE
             --coverage
             -O0
